8f7ac064d466090220bd3fa50e37672f36139701
hp <hp@MAHADEV>
2024-07-10 10:07:16 +0530

    my first commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javap_program {
+}
---
+++ src/string_program/demo1.java
@@ -0,0 +1,13 @@
+package string_program;
+
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		String k="kamesh";
+		System.out.println(k);
+		
+	}
+	
+	
+}
